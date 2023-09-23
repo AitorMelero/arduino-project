17f5225d080bfa30270179fbbc0f94b2d27d7073
AitorMelero <meleropiconaito@gmail.com>
2023-09-23 12:08:12 +0200

    ship: start model design

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+    return (
+        <>
+            <footer className="text-white static bottom-0">
+                <h1>Footer</h1>;
+            </footer>
+        </>
+    );
+};
