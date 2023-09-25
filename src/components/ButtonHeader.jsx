03e63b5b05147e18bff4a7b03ef936b5fc43a6af
AitorMelero <meleropiconaito@gmail.com>
2023-09-25 17:50:10 +0200

    ship: add basic styles in header including buttons

---
+++ src/components/ButtonHeader.jsx
@@ -0,0 +1,3 @@
+export const ButtonHeader = ({textButton = "Button"}) => {
+    return <button className="mx-10">{textButton}</button>;
+};
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
-export const Header = () => {
+import { ButtonHeader } from "./ButtonHeader";
+
+export const Header = ({ pageTitle = "Arduino Project" }) => {
     return (
-        <header className="text-white border border-white sticky h-24">
-            <h1 className="text-current text-4xl">Header</h1>
+        <header className="text-white border border-white sticky h-24 flex flex-row justify-between">
+            <h1 className="flex items-center text-4xl text-center">
+                {pageTitle}
+            </h1>
+            <div className="flex flex-row justify-center items-center">
+                <ButtonHeader textButton="About" />
+                <ButtonHeader textButton="Mode" />
+                <ButtonHeader textButton="GitHub" />
+            </div>
         </header>
     );
 };
