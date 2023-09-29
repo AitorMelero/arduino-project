2000fb1628cc2e85b540aeda252d5a55ccf0e670
AitorMelero <meleropiconaitor@gmail.com>
2023-09-29 17:23:09 +0200

    ship: dynamic dark and light mode

--- src/components/ButtonHeader.jsx
+++ src/components/ButtonHeader.jsx
@@ -2,9 +2,6 @@ export const ButtonHeader = ({ textButton = "Button" }) => {
     return (
         <a href="#" className="mx-10 text-base">
             {textButton}
-            {/* <i class="ti ti-brand-github-filled"></i> */}
-            {/* <i class="ti ti-sun"></i> */}
-            {/* <i class="ti ti-moon"></i> */}
         </a>
     );
 };
