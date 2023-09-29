4ab8063b9da9135ca8e30b6ff29e91801e0fdf58
AitorMelero <meleropiconaitor@gmail.com>
2023-09-29 21:53:18 +0200

    ship: add projects button in header to navigate to main page

--- src/components/ButtonHeader.jsx
+++ src/components/ButtonHeader.jsx
@@ -1,11 +1,15 @@
-export const ButtonHeader = ({ textButton = "Button", isSelected = false }) => {
+export const ButtonHeader = ({
+    textButton = "Button",
+    isSelected = false,
+    pathURL = "#",
+}) => {
     const classIsSelected = isSelected
         ? "bg-blue-2 text-blue"
         : "hover:bg-gray-4 hover:dark:bg-gray-3";
 
     return (
         <a
-            href="#"
+            href={pathURL}
             className={
                 "mx-10 text-base rounded-full px-3 py-2 " + classIsSelected
             }
