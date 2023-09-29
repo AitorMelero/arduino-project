0fb76b6806461f8eed7fbcdacdd04e7d9df949f6
AitorMelero <meleropiconaito@gmail.com>
2023-09-29 17:23:09 +0200

    ship: dynamic dark and light mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {},
+        colors: {
+            transparent: "transparent",
+            current: "currentColor",
+            primary: "#ffffff",
+            "primary-dark": "#23272f",
+        },
+    },
+    plugins: [],
+    darkMode: "class",
+};
