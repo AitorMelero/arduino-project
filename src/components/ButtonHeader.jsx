052f27a24d87cfd0aa0af158cc7095ecf3021f4b
AitorMelero <meleropiconaitor@gmail.com>
2023-09-29 13:02:28 +0200

    ship: start header implementation

--- src/components/ButtonHeader.jsx
+++ src/components/ButtonHeader.jsx
@@ -1,3 +1,10 @@
-export const ButtonHeader = ({textButton = "Button"}) => {
-    return <button className="mx-10">{textButton}</button>;
+export const ButtonHeader = ({ textButton = "Button" }) => {
+    return (
+        <a href="#" className="mx-10 text-base">
+            {textButton}
+            {/* <i class="ti ti-brand-github-filled"></i> */}
+            {/* <i class="ti ti-sun"></i> */}
+            {/* <i class="ti ti-moon"></i> */}
+        </a>
+    );
 };
