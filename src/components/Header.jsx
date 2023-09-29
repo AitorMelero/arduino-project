a80d8948a50942453a75d023889c91d2a725cd22
AitorMelero <meleropiconaitor@gmail.com>
2023-09-29 20:42:49 +0200

    ship: improved the load theme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,8 @@
-import { useEffect, useState } from "react";
 import { IconBrandGithubFilled, IconMoon, IconSun } from "@tabler/icons-react";
 
 import { ButtonHeader } from "./ButtonHeader";
 
 export const Header = ({ pageTitle = "Arduino Project" }) => {
-    const [theme, setTheme] = useState("light");
-
-    useEffect(() => {
-        let mainTheme = "light";
-
-        // Get the theme
-        if (
-            typeof localStorage !== "undefined" &&
-            localStorage.getItem("theme")
-        ) {
-            mainTheme = localStorage.getItem("theme");
-        }
-
-        if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-            mainTheme = "dark";
-        }
-
-        if (mainTheme === "dark") {
-            document.documentElement.classList.add("dark");
-        } else {
-            document.documentElement.classList.remove("dark");
-        }
-
-        window.localStorage.setItem("theme", mainTheme);
-        setTheme(mainTheme);
-    }, []);
-
     // Change the theme
     const handleToggleModeDark = () => {
         const element = document.documentElement;
@@ -39,7 +11,6 @@ export const Header = ({ pageTitle = "Arduino Project" }) => {
         const isDark = element.classList.contains("dark");
         const mainTheme = isDark ? "dark" : "light";
         localStorage.setItem("theme", mainTheme);
-        setTheme(mainTheme);
     };
 
     return (
@@ -49,9 +20,10 @@ export const Header = ({ pageTitle = "Arduino Project" }) => {
             </h1>
             <div className="flex flex-row justify-center items-center">
                 <ButtonHeader textButton="About" />
-                <a href="#" className="mx-10" onClick={handleToggleModeDark}>
-                    {theme === "dark" ? <IconSun /> : <IconMoon />}
-                </a>
+                <button className="mx-10" onClick={handleToggleModeDark}>
+                    <IconSun className="hidden dark:block" />
+                    <IconMoon className="block dark:hidden" />
+                </button>
                 <a href="#" className="mx-10">
                     <IconBrandGithubFilled />
                 </a>
