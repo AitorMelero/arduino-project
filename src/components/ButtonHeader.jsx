6448a1457e098ee82408b65af5cbe7577cc9fb6d
AitorMelero <meleropiconaitor@gmail.com>
2023-10-03 09:29:32 +0200

    fix: about path and warnings with dynamic header buttons styles

--- src/components/ButtonHeader.jsx
+++ src/components/ButtonHeader.jsx
@@ -1,6 +1,33 @@
-export const ButtonHeader = ({ id, textButton = "Button", pathURL = "#" }) => {
+import { useEffect, useState } from "react";
+
+export const ButtonHeader = ({
+    id,
+    textButton = "Button",
+    pathURL = "#",
+    generalStyles = "",
+}) => {
+    const [classStyles, setClassStyles] = useState(
+        generalStyles + "px-3 py-2 "
+    );
+
+    useEffect(() => {
+        const pathname = window.location.pathname;
+
+        // Check if the button is focus
+        if (
+            pathname === pathURL ||
+            (id === "projects" && pathname.split("/")[1] === "posts")
+        ) {
+            setClassStyles(classStyles + "bg-blue-2 text-blue");
+        } else {
+            setClassStyles(
+                classStyles + "hover:bg-gray-4 hover:dark:bg-gray-3"
+            );
+        }
+    }, []);
+
     return (
-        <a id={id} href={pathURL}>
+        <a id={id} href={pathURL} className={classStyles}>
             {textButton}
         </a>
     );
