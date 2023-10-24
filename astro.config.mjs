5d7a65cda2578c1b2a7dbc2af23c06698a577167
AitorMelero <meleropiconaitor@gmail.com>
2023-10-24 12:34:03 +0200

    ship: build web with Docker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), mdx()]
+  integrations: [react(), tailwind(), mdx()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
