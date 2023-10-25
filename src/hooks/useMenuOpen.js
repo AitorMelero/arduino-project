b0c2eed5c1960e123460b1bf0e9a834eccc5dbcd
AitorMelero <meleropiconaitor@gmail.com>
2023-10-25 11:33:04 +0200

    fix: error with article container positions in mobiles

--- src/hooks/useMenuOpen.js
+++ src/hooks/useMenuOpen.js
@@ -18,7 +18,7 @@ export const useMenuOpen = () => {
     const handleToggleMenu = () => {
         const menuContainer = document.getElementById("menu-content");
         const menuStyle =
-            "absolute top-[6rem] z-10 bg-white dark:bg-gray-1 text-gray-2 dark:text-white flex flex-col w-full h-[calc(100vh-6rem)] lg:hidden";
+            "absolute top-[6rem] z-10 bg-white dark:bg-gray-1 text-gray-2 dark:text-white flex flex-col w-full h-[calc(100%-6rem)] lg:hidden";
 
         if (menuIsOpen) {
             menuContainer.className = "hidden";
