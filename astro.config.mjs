3c9fa8efcac5f7c6222bda309bd8aaac99c9cc7a
AitorMelero <meleropiconaito@gmail.com>
2023-10-31 11:15:00 +0100

    release: V1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
-import node from "@astrojs/node";
 
 import vercel from "@astrojs/vercel/serverless";
 
